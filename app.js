5db4c6bb10b09c06e08405cde8f27f8d78226c92
heiner <heiner.giehl@yahoo.com>
2021-03-03 07:13:55 +0100

    ...

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const wrapperMain = document.getElementsByClassName('wrapperMain')[0]
 
 const loadingSpinnerAndLoadingDots = document.getElementsByClassName('loadingSpinnerAndLoadingDots')[0]
 window.addEventListener("load", function() {
-    wrapper.style.setProperty("display", "none")
+    wrapper.removeChild("loadingSpinnerAndLoadingDots")
  wrapperMain.style.setProperty("opacity", "1")
    
 })
